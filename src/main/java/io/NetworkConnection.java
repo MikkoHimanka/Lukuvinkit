b9c292228163114cdac9a67a8ad12853ff2d788d
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-03-05 22:09:13 +0200

    Implement checking program internet access

---
+++ src/main/java/io/NetworkConnection.java
@@ -0,0 +1,5 @@
+package io;
+import java.net.URL;
+public interface NetworkConnection {
+    boolean isConnected();
+}
---
+++ src/main/java/io/NetworkConnectionImpl.java
@@ -0,0 +1,51 @@
+package io;
+
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.lang.RuntimeException;
+import java.io.IOException;
+
+public class NetworkConnectionImpl implements NetworkConnection {
+    private URL onlineUrl;
+    private int timeout;
+
+    // `timeout` is the timeout set for establishing the connection and
+    // for reading the content.
+    // Does NOT include DNS lookup time!
+    public NetworkConnectionImpl(String onlineUrl, int timeout) throws MalformedURLException {
+        this.onlineUrl = new URL(onlineUrl);
+        this.timeout = timeout;
+    }
+    public boolean isConnected() throws RuntimeException {
+        HttpURLConnection connection;
+        try {
+            connection = openTimeoutConnection(onlineUrl);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        try {
+            // Use HEAD to download less stuff
+            connection.setRequestMethod("HEAD");
+        } catch (ProtocolException ex) {
+            throw new RuntimeException(ex);
+        }
+        return httpOk(connection);
+    }
+    private boolean httpOk(HttpURLConnection connection) {
+        try {
+            int response = connection.getResponseCode();
+            return response == HttpURLConnection.HTTP_OK;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+    private HttpURLConnection openTimeoutConnection(URL url) throws IOException {
+        HttpURLConnection connection;
+        connection = (HttpURLConnection) url.openConnection();
+        connection.setConnectTimeout(this.timeout);
+        connection.setReadTimeout(this.timeout);
+        return connection;
+    }
+}
